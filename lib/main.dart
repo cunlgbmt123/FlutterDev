92f5509f6b0c72ff8929fc6e1382f1ae6c33593d
cunlgbmt123 <tieulong.8b@gmail.com>
2022-07-20 09:18:52 +0700

    commit top navbar

--- lib/main.dart
+++ lib/main.dart
@@ -13,19 +13,55 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('First Application'),
+          leading: const Icon(Icons.menu),
+          title: const Text('this is home page'),
+          actions: const [
+            Icon(Icons.favorite),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 16),
+              child: Icon(Icons.search),
+            ),
+            Icon(Icons.more_vert),
+          ],
+          backgroundColor: Colors.pink,
         ),
-        body: const Text('this is body'),
+        body: const Text('this is body where your can display widget'),
+        // this is bottom navigationbar
         bottomNavigationBar: BottomNavigationBar(
           items: const [
             BottomNavigationBarItem(
               label: 'Home',
-              icon: Icon(Icons.home),
+              icon: Icon(
+                Icons.home,
+                color: Colors.pink,
+                size: 24.0,
+                semanticLabel: 'this is home button',
+              ),
             ),
             BottomNavigationBarItem(
               label: 'Setting',
-              icon: Icon(Icons.settings),
+              icon: Icon(
+                Icons.settings,
+                color: Colors.pink,
+                size: 24.0,
+                semanticLabel: 'this is setting button',
+              ),
             ),
+            BottomNavigationBarItem(
+              label: 'Love',
+              icon: Icon(
+                Icons.favorite_sharp,
+                color: Colors.pink,
+                size: 24.0,
+              ),
+            ),
+            BottomNavigationBarItem(
+              label: 'Button',
+              icon: Icon(
+                Icons.access_alarm,
+                color: Colors.pink,
+              ),
+            )
           ],
         ),
       ),
